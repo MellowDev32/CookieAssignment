edf3ff4a23de668993454de2fb1dd30472d3c977
Mellow <cpryan@mail.fhsu.edu>
2021-03-28 13:01:01 -0500

    possible fix to db file

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
     // Heroku connection
-    $dsn = 'u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=wqynmrlu0rnt5epv';
+    $dsn = 'mysql:host=u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=wqynmrlu0rnt5epv';
     $username = 'h0pv4uzkya2kb1o5';
     $password = 'uszhmvpb8vqd4i7m'; 
     
